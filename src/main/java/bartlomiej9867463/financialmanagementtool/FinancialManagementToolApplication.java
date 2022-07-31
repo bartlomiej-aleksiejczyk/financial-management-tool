126c0cf56025b5ccd3270e02a35a1b906ad6950c
Jan Kowalski <dawid174655@hotmail.com>
2022-07-31 12:58:11 +0200

    Added auto generated springboot files

---
+++ src/main/java/bartlomiej9867463/financialmanagementtool/FinancialManagementToolApplication.java
@@ -0,0 +1,13 @@
+package bartlomiej9867463.financialmanagementtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialManagementToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bartlomiej9867463/financialmanagementtool/FinancialManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package bartlomiej9867463.financialmanagementtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
